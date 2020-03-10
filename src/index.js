58086b850daede43e3ae350edb63cf45f5b24330
pathas <pathas@naver.com>
2020-03-10 23:40:02 +0900

    Start Redux in VanillaJS

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
+
+countStore.dispatch({ type: "ADD" });
+console.log(countStore.getState());
